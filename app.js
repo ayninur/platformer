b9d01178da203b0b9c593e6ac203af91dd994854
ayninur <nuralaynabdulqadir@gmail.com>
2020-12-25 23:15:43 -0500

    creating spritesheet

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+///////////////////////////////////////////////
+// CREATE ASSET LOADER - loads and access imgs
+//////////////////////////////////////////////
+
 // wrap inside immediately invoked function expression (IIFE)
 (function () {
     // get canvas context so we can use them
@@ -97,3 +101,25 @@
         startGame();
     }
 })();
+
+////////////////////////////////////////////////////////
+// SPITESHEET - Sprite animation for our main character
+////////////////////////////////////////////////////////
+
+/**
+ * Create a Spritesheet
+ * @param {string} - Path to the image
+ * @param {number} - Width (in px) of each frame.
+ * @param {number} - Height (in px) of each frame.
+ */
+
+function Spritesheet(path, frameWidth, frameHeight) {
+    this.image = new Image();
+    this.frameWidth = frameWidth;
+    this.frameHeight = frameHeight;
+    // calculate the number of frames in a row after the image loads
+    var self = this;
+    this.image.onload = function () {
+        self.framesPerRow = Math.floor(self.image.width)
+    }
+}
